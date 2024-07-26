6e59222a420e38f18707cbca001bb7e3aa34407a
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-07-26 11:41:22 +0530

    Completed the folder structure and styled component learning part

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.type === "h1" &&
+    css`
+      font-size: 80px;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.type === "h2" &&
+    css`
+      font-size: 50px;
+      font-weight: 500;
+    `}
+
+    ${(props) =>
+    props.type === "h3" &&
+    css`
+      font-size: 30px;
+      font-weight: 400;
+    `}
+
+    line-height: 100px
+`;
+
+export default Heading;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 10px;
+  border: 2px solid var(--color-grey-400);
+  border-radius: var(--border-radius-sm);
+`;
+export default Input;
