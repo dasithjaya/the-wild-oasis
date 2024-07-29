9afd2def455aaf8abf25ec0177f84efa5e4e8b91
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-07-29 22:18:39 +0530

    Integrated reac-query with the application and added react-toast

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,15 +4,23 @@ import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
 const StyledAppLayout = styled.div`
-    display: grid;
-    grid-template-columns: 26rem 1fr;
-    grid-template-rows: auto 1fr;
-    height: 100vh;
-`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+`;
+
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 export default function AppLayout() {
@@ -21,7 +29,9 @@ export default function AppLayout() {
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
