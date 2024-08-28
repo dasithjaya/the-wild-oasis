79e5286888a31d87f55866990239cb5680dea747
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-08-28 18:49:11 +0530

    Completed checkout feature

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,7 +23,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const navigate = useNavigate();
   const { isLoading, booking } = useBooking();
-
+  const { checkout, isCheckOut } = useCheckout();
   const moveBack = useMoveBack();
 
   const statusToTagName = {
@@ -50,6 +51,15 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+
+        {status === "checked-in" && (
+          <Button
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckOut}
+          >
+            Check out
           </Button>
         )}
         <Button variation="secondary" onClick={moveBack}>
--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -9,7 +9,8 @@ import { formatDistanceFromNow } from "../../utils/helpers";
 import Menus from "../../ui/Menus";
 import { HiEye } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
-import { HiArrowDownOnSquare } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -53,6 +54,8 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+
+  const { checkout, isCheckOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -102,6 +105,16 @@ function BookingRow({
               onClick={() => navigate(`/checkin/${bookingId}`)}
             >
               Check in
+            </Menus.Button>
+          )}
+
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckOut}
+            >
+              Check out
             </Menus.Button>
           )}
         </Menus.List>
