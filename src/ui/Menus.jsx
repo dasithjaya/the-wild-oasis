5bcf998ebe581cbc832f58d7e4c4c334f51e1e50
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2024-09-03 10:31:50 +0530

    Fixed some bugs and completed the project

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -88,6 +88,7 @@ function Toggle({ id }) {
   const { openId, open, close, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
     const rect = e.target.closest("button").getBoundingClientRect();
     // console.log(rect);
     setPosition({
@@ -106,7 +107,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useClickOutside(close);
+  const ref = useClickOutside(close, false);
 
   if (openId !== id) return null;
 
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Logo from './Logo'
 import MainNav from './MainNav'
-import Uploader from "../data/Uploader";
+// import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -17,6 +17,6 @@ export default function Sidebar() {
   return <StyledSidebar>
     <Logo />
     <MainNav />
-    <Uploader/>
+    {/* <Uploader/> */}
   </StyledSidebar>;
 }
